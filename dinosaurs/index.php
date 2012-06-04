ece73dfbf1a3d21abc035d67a15aca36ad728816
jasp0003 <prabhjot404@gmail.com>
2012-06-04 14:16:06 -0400

    added dino list

--- dinosaurs/index.php
+++ dinosaurs/index.php
@@ -1,4 +1,19 @@
-<!DOCTYPE HTML>
+<?php
+
+require_once 'includes/db.php';
+
+$sql = $db->query('
+    SELECT id, dino_name, loves_meat, in_jurassic_park
+    FROM dinosaurs
+    ORDER BY dino_name ASC
+');
+
+// Display the last error created by our database
+var_dump($db->errorInfo());
+$results = $sql->fetchAll();
+
+
+?><!DOCTYPE HTML>
 <html>
 <head>
 <meta charset="utf-8">
@@ -6,13 +21,21 @@
 </head>
 <body>
 
- 
-    <h2>Velociraptor</h2>
+    <?php foreach ($results as $dino): ?>
+    <h2>
+        <a href="single.php?id=<?php echo $dino['id']; ?>">
+		<?php echo $dino['dino_name']; ?>
+        </a>
+         </h2>
+         
+    
     <dl>
       <dt>Loves Meat</dt>
-      <dd>1</dd>
+      <dd><?php echo $dino['loves_meat']; ?></dd>
       <dt>In Jurassic Park</dt>
-      <dd>1</dd>
+      <dd><?php echo $dino['in_jurassic_park']; ?></dd>
+      </dl>
+      <?php endforeach; ?>
 
 </body>
 </html>

14bfb836d0320a4ca182bdc9207ccb80051c6a1b
jasp0003 <prabhjot404@gmail.com>
2012-06-21 13:20:26 -0400

    whole assignment of ajax tabs on theme "planets" including four planets html files , one main index.html file, one css file, one js file.

---
+++ mtm1526/assignment-4/js/planets-in-ajax.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	
+	
+	
+	$('#gethtml').on('click',function(){
+		$('fillme').load('earth.html');
+		
+	});
+	
+	$('#gethtml').on('click',function(){
+		$('fillme').load('mars.html');
+		
+	});
+	
+	$('#gethtml').on('click',function(){
+		$('fillme').load('jupiter.html');
+		
+	});
+	
+	$('#gethtml').on('click',function(){
+		$('fillme').load('pluto.html');
+		
+	});
+	
+});
---
+++ mtm1526/assignment-4/jupiter.html
@@ -0,0 +1,2 @@
+<h1>JUPITER</h1>
+<h2>Jupiter is the biggest planet</h2>
